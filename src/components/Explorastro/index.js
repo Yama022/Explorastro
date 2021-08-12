b566b4d75e77bb1d69393a416c03a6db8882336d
JebNewman <pierre.f.laine@gmail.com>
2021-08-12 17:06:31 +0200

    OK| Header WIP| Footer

--- src/components/Explorastro/index.js
+++ src/components/Explorastro/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
 
 export default function Explorastro() {
   return (
     <div className="explorastro">
+      <Header />
       <Footer />
     </div>
   );
