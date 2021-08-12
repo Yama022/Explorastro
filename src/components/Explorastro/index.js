cf381d5f25e4c0868608678869a4e5532259aaff
JebNewman <pierre.f.laine@gmail.com>
2021-08-12 11:58:52 +0200

    OK| vars & main component

---
+++ src/components/Explorastro/index.js
@@ -0,0 +1,15 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import 'src/scss/index.scss';
+
+// == Composant
+const Explorastro = () => (
+  <div className="explorastro">
+    <h1>ExplorAstro</h1>
+  </div>
+);
+
+// == Export
+export default Explorastro;
