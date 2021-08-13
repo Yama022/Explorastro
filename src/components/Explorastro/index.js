6ae28ae46e7877702b669affe63e5bbe607a8137
Théo BIET <dev.theobiet@gmail.com>
2021-08-13 13:53:04 +0200

    Clean master

--- src/components/Explorastro/index.js
+++ src/components/Explorastro/index.js
@@ -3,7 +3,6 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
-import Main from 'src/components/Main';
 import Timeline from 'src/components/Timeline';
 import Error from 'src/components/Error';
 
@@ -11,7 +10,6 @@ export default function Explorastro() {
   return (
     <div className="explorastro">
       <Header />
-      <Main />
       <Switch>
         <Route exact path="/">
           <Redirect to="/timeline" />
