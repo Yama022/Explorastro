c0beef41d69481c71d121f4a8c4fcc82c18be1f8
Théo BIET <dev.theobiet@gmail.com>
2021-08-13 14:54:13 +0200

    Create seeding file

---
+++ api/migrations/seeding.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const client = require("../app/client");
+const faker = require("faker");
+
+const config = {
+  MAX_USER: 500,
+  MAX_EXPLORATION: 100,
+};
+
+const insertUsers = async () => {};
+const insertExplorations = async () => {};
