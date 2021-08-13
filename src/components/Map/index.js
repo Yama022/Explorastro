9a71ba16ec8c6fa4432c2012b1994ff6540a5eb2
baptistefaidherbe <bapt78@gmail.com>
2021-08-13 23:48:20 +0200

    Update map + markers

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,26 +1,54 @@
 import React from 'react';
 import './style.scss';
-import { Icon } from 'leaflet';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
 import {
   MapContainer,
   TileLayer,
   Marker,
   Popup,
 } from 'react-leaflet';
+import coord from './data2.json';
+import img_event from'./bg.png';
 
 export default function Map() {
+  coord.forEach((element) => {
+    element.desc = 'Venez decouvrir le ciel samedi 14/08/2021 !! ça va être trop bien !!';
+    element.title = 'sortie astro entre poto !!';
+  });
   return (
     <div className="map">
-      <MapContainer className="test" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <div className="list_events">
+        {coord.map((x) => (
+          <div className="list_events__box">
+            <img src={img_event} alt="" />
+            <div className="list_events__box__content">
+              <h2>{x.title}</h2>
+              <p>{x.desc}</p>
+              <button>Participer</button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <MapContainer
+        center={[46.232192999999995, 2.209666999999996]}
+        zoom={6.4}
+        maxZoom={18}
+        minZoom={6.4}
+      >
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        <MarkerClusterGroup>
+          {coord.map((x) => (
+            <div>
+              <Marker position={[x.Latitude, x.Longitude]}>
+                <Popup>Exploration vers {x.name}</Popup>
+              </Marker>
+            </div>
+          ))}
+
+        </MarkerClusterGroup>
       </MapContainer>
     </div>
   );
