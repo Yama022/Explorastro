348a1aab7c89673b105b1a36ad2c1553e30730ec
Théo BIET <dev.theobiet@gmail.com>
2021-08-13 18:36:41 +0200

    Timeline created

---
+++ src/components/Timeline/Discover/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import night from 'src/assets/images/night.jpg';
+
+export default function Discover() {
+  return (
+    <div className="timeline-discover">
+      <img src={night} alt="Night Earth" />
+      <p>Découvrir</p>
+    </div>
+  );
+}
--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Discover from './Discover';
 
 export default function Timeline() {
   return (
     <div className="timeline">
-      <h1 className="main-title">Your Timeline</h1>
+      <aside className="timeline-left">
+        <NavLink to="/discover" className="timeline-left__widget">
+          <Discover />
+        </NavLink>
+      </aside>
+      <main className="timeline-main">
+        <div className="timeline-main__exploration"><h1>Exploration</h1></div>
+      </main>
+      <aside className="timeline-right">
+        <div className="timeline-left__widget">
+          Widget
+        </div>
+      </aside>
     </div>
   );
 }
