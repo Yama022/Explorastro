de823f6e4c9f675b1f530d9e328c2696a98a27a6
Remi Jacquemin <r-jacquemin@hotmail.fr>
2021-08-13 11:43:33 +0200

    Création du components Main & ajout du scss

--- src/components/Explorastro/index.js
+++ src/components/Explorastro/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
+import Main from 'src/components/Main';
 
 export default function Explorastro() {
   return (
     <div className="explorastro">
       <Header />
+      <Main />
       <Footer />
     </div>
   );
---
+++ src/components/Main/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Main() {
+  return (
+    <main className="main">
+      <h1>Coucou</h1>
+      <h1>Coucou</h1>
+      <h1>Coucou</h1>
+    </main>
+  );
+}
