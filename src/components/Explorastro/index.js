678085d1ecc91cd0191086e02da0c666f8f76385
JebNewman <pierre.f.laine@gmail.com>
2021-08-13 17:35:38 +0200

    OK| Login page TODO|parallax OK| routes fixed

--- src/components/Explorastro/index.js
+++ src/components/Explorastro/index.js
@@ -3,27 +3,33 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
-import Main from 'src/components/Main';
 import Timeline from 'src/components/Timeline';
 import Error from 'src/components/Error';
+import Login from 'src/components/Login';
 
 export default function Explorastro() {
   return (
     <div className="explorastro">
-      <Header />
-      <Main />
       <Switch>
         <Route exact path="/">
+          <Header />
           <Redirect to="/timeline" />
+          <Footer />
         </Route>
         <Route exact path="/timeline">
+          <Header />
           <Timeline />
+          <Footer />
+        </Route>
+        <Route exact path="/login">
+          <Login />
         </Route>
         <Route>
+          <Header />
           <Error />
+          <Footer />
         </Route>
       </Switch>
-      <Footer />
     </div>
   );
 }
