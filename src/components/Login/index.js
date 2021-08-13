ab2f14485658364052f627080e50ba59c41c0b14
Théo BIET <dev.theobiet@gmail.com>
2021-08-13 19:26:16 +0200

    Parallax OK, & Login Form too

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,51 +1,65 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Parallax, Background } from 'react-parallax';
-
 import logo from 'src/assets/images/logo-explorastro.png';
-
-import mascot from 'src/assets/images/mascot-rocket.svg';
-import mascotBg from 'src/assets/images/mascot-background.svg';
+import { FaUser } from 'react-icons/fa';
 
 export default function Login() {
+  useEffect(() => {
+    (function () {
+      const elem = document.querySelector('#parallax');
+
+      function parallax(e) {
+        const w = window.innerWidth / 2;
+        const h = window.innerHeight / 2;
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+        const depth1 = `${50 - ((mouseX - w) * 0.01)}% ${50 - ((mouseY - h) * 0.01)}%`;
+        const depth2 = `${50 - ((mouseX - w) * 0.02)}% ${50 - ((mouseY - h) * 0.02)}%`;
+        const depth3 = `${50 - ((mouseX - w) * 0.06)}% ${50 - ((mouseY - h) * 0.06)}%`;
+        const x = `${depth3}, ${depth2}, ${depth1}`;
+        elem.style.backgroundPosition = x;
+      }
+
+      document.addEventListener('mousemove', parallax);
+    }());
+  });
+
+  const [login, setLogin] = React.useState("");
+
   return (
     <div className="login">
-      <div className="login__images" id="login__images__scene">
-        <img src={mascotBg} alt="Planète derrière la mascotte" className="login__images__mascotbg" />
-        <img src={mascot} alt="Mascotte sur une fusée" className="login__images__mascot" />
+      <div className="login__illustration">
+        <div id="parallax" />
       </div>
-      <div className="login__main-content">
-        <header className="login__main-content__header">
-          <Link to="/">
-            <img src={logo} alt="Logo Explorastro" className="login__main-content__header__logo" />
-          </Link>
-          <Link to="/" className="login__main-content__header__accueil">Retourner à l'accueil</Link>
-        </header>
-        <form action="POST" className="login__main-content__form">
-          <div className="login__main-content__form__elem">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label htmlFor="login-mail">Adresse mail</label>
-            <input className="login__main-content__form__elem__input" id="login-mail" type="email" />
+      <div className="login__container">
+        <div className="login__container__form">
+          <h2>Connexion</h2>
+          <form action="">
+            <div className="field">
+              <label className="label">Username</label>
+              <div className="control has-icons-left has-icons-right">
+                <input className="input is-success" type="text" placeholder="ex: AstroCharles" value={login} name="login" onChange={(e) => setLogin(e.target.value)} />
+                <span className="icon is-small is-left">
+                  <FaUser />
+                </span>
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Mot de Passe</label>
+              <div className="control has-icons-left has-icons-right">
+                <input className="input is-success" type="password" placeholder="ex: MyStr0ngP455sWoRD" value={login} name="login" onChange={(e) => setLogin(e.target.value)} />
+                <span className="icon is-small is-left">
+                  <FaUser />
+                </span>
+              </div>
+            </div>
+          </form>
+          <div className="login__container__form__buttons-container">
+            <div className="button purple">Se Connecter</div>
+            <div className="button --outlined">S'inscrire</div>
           </div>
-          <div className="login__main-content__form__elem">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label htmlFor="login-password">Mot de passe</label>
-            <input className="login__main-content__form__elem__input" id="login-password" type="password" />
-          </div>
-          <div>
-            <input type="checkbox" id="login-remember" name="remember" />
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label htmlFor="login-remember" className="login__main-content__form__remember">Se souvenir de moi</label>
-          </div>
-          <div className="login__main-content__form__submit">
-            <button type="submit" className="login__main-content__form__submit__button login__main-content__form__submit__button--signin">
-              Se connecter
-            </button>
-            <button type="submit" className="login__main-content__form__submit__button login__main-content__form__submit__button--signup">
-              S'inscrire
-            </button>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
   );
