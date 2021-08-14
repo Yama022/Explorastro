e2b063e1a06055d96c441f42240f8a76e81796ee
JebNewman <pierre.f.laine@gmail.com>
2021-08-14 11:38:09 +0200

    FIX| footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,18 +2,17 @@ import React from 'react';
 
 import logo from 'src/assets/images/logo-explorastro.png';
 
+import moon from 'src/assets/images/cropped-moon-footer.svg';
+
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="footer__container">
-        <img src={logo} alt="Logo ExplorAstro" className="footer__container__logo" />
-        <div className="footer__container__description">
-          <a href="#">CONDITIONS D'UTILISATION</a>
-          <a href="#">A PROPOS</a>
-          <a href="#">AIDE</a>
-          <p className="footer__container__copyright">Copyright 2021 ExplorAstro</p>
-        </div>
-
+      <img src={logo} alt="Logo ExplorAstro" className="footer__logo" />
+      <div className="footer__description">
+        <a href="#">CONDITIONS D'UTILISATION</a>
+        <a href="#">A PROPOS</a>
+        <a href="#">AIDE</a>
+        <p className="footer__copyright">Copyright 2021 ExplorAstro</p>
       </div>
     </footer>
   );
