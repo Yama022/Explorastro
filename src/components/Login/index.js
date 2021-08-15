55ef208bc2ce191d71cdb5802c9ce3632e8c4f3c
Théo BIET <dev.theobiet@gmail.com>
2021-08-15 20:13:56 +0200

    Review parallax. To Review, bu v1 work

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,38 +1,49 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useEffect } from 'react';
+import planet1 from 'src/assets/images/login/1.png';
+import planet2 from 'src/assets/images/login/2.png';
+import planet3 from 'src/assets/images/login/3.png';
+import planet4 from 'src/assets/images/login/4.png';
+import planet5 from 'src/assets/images/login/5.png';
+import planet6 from 'src/assets/images/login/6.png';
+import planet7 from 'src/assets/images/login/7.png';
+import planet8 from 'src/assets/images/login/8.png';
+import planet9 from 'src/assets/images/login/9.png';
+import logo from 'src/assets/images/logo-explorastro.png';
+
 import { FaUser } from 'react-icons/fa';
 
 export default function Login() {
   useEffect(() => {
-    (function () {
-      const elem = document.querySelector('#parallax');
+    function parallax(e) {
+      document.querySelectorAll('.object').forEach((move) => {
+        const movingValue = move.getAttribute('data-value');
+        const x = (e.clientX * movingValue) / 250;
+        const y = (e.clientY * movingValue) / 250;
 
-      function parallax(e) {
-        const w = window.innerWidth / 2;
-        const h = window.innerHeight / 2;
-        const mouseX = e.clientX;
-        const mouseY = e.clientY;
-        const depth1 = `${50 - ((mouseX - w) * 0.01)}% ${50 - ((mouseY - h) * 0.01)}%`;
-        const depth2 = `${50 - ((mouseX - w) * 0.02)}% ${50 - ((mouseY - h) * 0.02)}%`;
-        const depth3 = `${50 - ((mouseX - w) * 0.06)}% ${50 - ((mouseY - h) * 0.06)}%`;
-        const x = `${depth3}, ${depth2}, ${depth1}`;
-        elem.style.backgroundPosition = x;
-      }
+        move.style.transform = `translateX(${x}px) translateY(${y}px)`;
+      });
+    }
 
-      document.addEventListener('mousemove', parallax);
-    }());
+    document.addEventListener('mousemove', parallax);
   });
 
   const [login, setLogin] = React.useState('');
 
   return (
     <div className="login">
-      <div className="login__illustration">
-        <div id="parallax" />
-      </div>
+      <img src={planet1} className="object" data-value="-2" alt="" />
+      <img src={planet2} className="object" data-value="6" alt="" />
+      <img src={planet3} className="object" data-value="4" alt="" />
+      <img src={planet4} className="object" data-value="-6" alt="" />
+      <img src={planet5} className="object" data-value="8" alt="" />
+      <img src={planet6} className="object" data-value="-4" alt="" />
+      <img src={planet7} className="object" data-value="5" alt="" />
+      <img src={planet8} className="object" data-value="-9" alt="" />
+      <img src={planet9} className="object" data-value="-5" alt="" />
       <div className="login__container">
         <div className="login__container__form">
-          <h2>Connexion</h2>
+          <img src={logo} alt="" />
           <form action="">
             <div className="field">
               <label className="label">Username</label>
@@ -53,6 +64,7 @@ export default function Login() {
               </div>
             </div>
           </form>
+
           <div className="login__container__form__buttons-container">
             <div className="button purple">Se Connecter</div>
             <div className="button --outlined">S'inscrire</div>
