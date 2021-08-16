748f48d15c09dcb25db44b4b568a7d8d835d3127
Théo BIET <dev.theobiet@gmail.com>
2021-08-16 10:40:47 +0200

    🧍 /user/:id getInformations method OK

---
+++ api/app/constants/error.js
@@ -0,0 +1,4 @@
+module.exports = {
+    INTERNAL_ERROR: "Internal server error, please retry in a few minutes",
+    USER_NOT_FOUND: "User not found",
+}
--- api/app/controllers/user.js
+++ api/app/controllers/user.js
@@ -1,6 +1,25 @@
+const { User } = require("../models");
+const { errorMessage } = require("../constants");
+
 module.exports = {
-  getInformations: (req, res) => {
-    res.json({ message: "user" });
+  getInformations: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const user = await User.findByPk(id);
+
+      console.log(user);
+
+      if (!user) {
+        return res.status(404).json({ message: errorMessage.USER_NOT_FOUND });
+      }
+
+      return res.status(200).send(user);
+    } catch (error) {
+      res.status(400).json({
+        message: errorMessage.internal_error,
+      });
+    }
   },
 
   update: (req, res) => {
