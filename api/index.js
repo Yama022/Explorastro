3e7c4c9970208d510de85892dc2a03481c1b3766
Théo BIET <dev.theobiet@gmail.com>
2021-08-18 13:47:30 +0200

    📚 Setting up SwaggerUI Documentation

--- api/index.js
+++ api/index.js
@@ -4,14 +4,39 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3003;
 const router = require("./app/routers");
+const expressSwagger = require('express-swagger-generator')(app);
 
 app.use(cors());
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.redirect("/api/v1/");
-});
+const options = {
+  swaggerDefinition: {
+    info: {
+      description: 'Official documentation of the ExplorAstro API for my love Front-End Developers',
+      title: 'ExplorAstro-API',
+      version: '0.7-alpha',
+    },
+    host: 'https://explorastro-api.herokuapp.com',
+    basePath: '/api/v1/',
+    produces: [
+      "application/json"
+    ],
+    schemes: ['http', 'https'],
+    securityDefinitions: {
+      JWT: {
+        type: 'apiKey',
+        in: 'header',
+        name: 'Authorization',
+        description: "",
+      }
+    }
+  },
+  basedir: __dirname,
+  files: ['./app/controllers/**/*.js']
+};
+
+expressSwagger(options);
 
 app.use("/api/v1", router);
 
