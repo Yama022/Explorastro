ada2c426b8f76b6a6bb43fdc99b670898a919d44
baptistefaidherbe <bapt78@gmail.com>
2021-08-20 10:53:29 +0200

    Add label published in field isPusblished + modif scss input image + add footer/header to formEvent

--- src/components/CreateEvent/FormEvent.js
+++ src/components/CreateEvent/FormEvent.js
@@ -3,7 +3,8 @@ import React, { useEffect } from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import * as dayjs from 'dayjs';
 import PropTypes from 'prop-types';
-// import logo from 'src/assets/images/placeholder_image.jpg';
+import Header from 'src/containers/Header';
+import Footer from 'src/components/Footer';
 import ControlGeocoder from './controlGeocoder';
 
 export default function FormEvent({
@@ -17,6 +18,7 @@ export default function FormEvent({
   dateEvent,
   maxRateEvent,
   descEvent,
+  published,
 
 }) {
   useEffect(() => {
@@ -41,106 +43,109 @@ export default function FormEvent({
   // };
 
   return (
-    <div className="container">
-      <h1 className="main-title">Créer un événement</h1>
-      <div className="createEvent">
+    <>
+      <Header />
+      <div className="container">
+        <h1 className="main-title">Créer un événement</h1>
+        <div className="createEvent">
 
-        <form className="form__create" onSubmit={handleSubmit}>
-          <h4 className="form__create__title">Nom de l'événement</h4>
+          <form className="form__create" onSubmit={handleSubmit}>
+            <h4 className="form__create__title">Nom de l'événement</h4>
 
-          <input
-            className="input is-link is-small"
-            name="titleEvent"
-            type="text"
-            value={titleEvent}
-            onChange={handleOnchange}
-            placeholder="Ex : Soirée nuit des étoiles"
-          />
-
-          <h4 className="form__create__title">Descirption</h4>
-          <textarea
-            name="descEvent"
-            id="txtArea"
-            rows="10"
-            cols="70"
-            onChange={handleOnchange}
-            placeholder="Ex : J'organise une soirée pour la nuit des étoiles dans un endroit bien connu vers chez moi..."
-            value={descEvent}
-          />
-
-          <h4 className="form__create__title">Date de l'événement</h4>
-          <input
-            className="input is-link is-small"
-            type="datetime-local"
-            name="dateEvent"
-            value={dayjs(dateEvent).format('YYYY-MM-DDTHH:mm:ss')}
-            min={dayjs().format('YYYY-MM-DDTHH:mm:ss')}
-            onChange={handleOnchange}
-          />
-
-          {/* <div className="form__right"> */}
-          <div className="form__map">
-            <MapContainer
-          // Centering on the map of france
-              center={[44.332192999999995, 1.309666999999996]}
-              zoom={6}
-              maxZoom={18}
-              minZoom={3}
-            >
-
-              {/* Add layer dark map */}
-              <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-                name="tiles"
-              />
-              {/* Add Markers events astro on the map */}
-              <ControlGeocoder coordLocation={getCoordLocation} />
-            </MapContainer>
-          </div>
-
-          <h4 className="form__create__title">Nombre de personne(s) maximum
             <input
-              value={maxRateEvent}
-              className="maxRateEvent"
-              name="maxRateEvent"
-              type="number"
+              className="input is-link is-small"
+              name="titleEvent"
+              type="text"
+              value={titleEvent}
               onChange={handleOnchange}
-              placeholder="0"
+              placeholder="Ex : Soirée nuit des étoiles"
             />
-          </h4>
 
-          <input type="checkbox" name="published" className="checkbox" id="checkbox" onClick={handleOnClick} />
-          <label className="published" htmlFor="checkbox" />
+            <h4 className="form__create__title">Descirption</h4>
+            <textarea
+              name="descEvent"
+              id="txtArea"
+              rows="10"
+              cols="70"
+              onChange={handleOnchange}
+              placeholder="Ex : J'organise une soirée pour la nuit des étoiles dans un endroit bien connu vers chez moi..."
+              value={descEvent}
+            />
 
-          <div className="form__add__img">
+            <h4 className="form__create__title">Date de l'événement</h4>
+            <input
+              className="input is-link is-small"
+              type="datetime-local"
+              name="dateEvent"
+              value={dayjs(dateEvent).format('YYYY-MM-DDTHH:mm:ss')}
+              min={dayjs().format('YYYY-MM-DDTHH:mm:ss')}
+              onChange={handleOnchange}
+            />
 
-            <div className="form__add__img__import">
-              <h4 className="form__add__img__title">Souhaitez-vous ajouter une image ? </h4>
-              <div className="button-wrapper">
+            {/* <div className="form__right"> */}
+            <div className="form__map">
+              <MapContainer
+          // Centering on the map of france
+                center={[44.332192999999995, 1.309666999999996]}
+                zoom={6}
+                maxZoom={18}
+                minZoom={3}
+              >
 
-                <input
-                  value={eventCreated.imageUrl}
-                  accept="image/*"
-                  // onChange={onImageChange}
-                  id="contained-button-file"
-                  multiple
-                  name="image"
-                  type="file"
-                  className="upload-box"
+                {/* Add layer dark map */}
+                <TileLayer
+                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+                  name="tiles"
                 />
-              </div>
+                {/* Add Markers events astro on the map */}
+                <ControlGeocoder coordLocation={getCoordLocation} />
+              </MapContainer>
             </div>
 
-          </div>
-          <div className="form__create__button">
-            <button className="button is-link " type="submit">Créer</button>
-            <button className="button is-danger" type="button">Annuler</button>
-          </div>
-        </form>
-      </div>
-    </div>
+            <h4 className="form__create__title">Nombre de personne(s) maximum
+              <input
+                value={maxRateEvent}
+                className="maxRateEvent"
+                name="maxRateEvent"
+                type="number"
+                onChange={handleOnchange}
+                placeholder="0"
+              />
+            </h4>
 
+            <input type="checkbox" name="published" className="checkbox" id="checkbox" onClick={handleOnClick} />
+            <label className="published" htmlFor="checkbox">{published ? 'Publié' : 'Non publié' } </label>
+
+            <div className="form__add__img">
+
+              <div className="form__add__img__import">
+                <h4 className="form__add__img__title">Souhaitez-vous ajouter une image ? </h4>
+                <div className="button-wrapper">
+
+                  <input
+                    value={eventCreated.imageUrl}
+                    accept="image/*"
+                  // onChange={onImageChange}
+                    id="contained-button-file"
+                    multiple
+                    name="image"
+                    type="file"
+                    className="upload-box"
+                  />
+                </div>
+              </div>
+
+            </div>
+            <div className="form__create__button">
+              <button className="button is-link " type="submit">Créer</button>
+              <button className="button is-danger" type="button">Annuler</button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
 
@@ -156,4 +161,5 @@ FormEvent.propTypes = {
   dateEvent: PropTypes.string.isRequired,
   maxRateEvent: PropTypes.number.isRequired,
   descEvent: PropTypes.string.isRequired,
+  published: PropTypes.bool.isRequired,
 };
