70e0c2e3018695f74cd9b165bf6656759daf5236
Etudiant O'clock <r-jacquemin@hotmail.fr>
2021-08-22 23:17:54 +0000

    OK | Modification component Guide

--- src/components/Guide/index.js
+++ src/components/Guide/index.js
@@ -72,11 +72,11 @@ export default function Guide() {
       {/* Création du 1er paragaphe pour expliquer ce qu'il est possible de voir suivant l'instrument utilisé */}
       <div className="guide__section">
         <Link to="/guide/visuel">
-          <h2 className="guide__section__title"><FcAdvance /> On peut aussi utiliser une lunette ou un téléscope pour du visuel !</h2>
+          <h2 className="guide__section__title"><FcAdvance /> On peut aussi utiliser une lunette ou un téléscope pour du visuel ! Clique sur moi pour voir ça !</h2>
         </Link>
 
         <Link to="/guide/photo">
-          <h2 className="guide__section__title"><FcAdvance /> Tu voudrais savoir comment on fait de la photo astro ? :D</h2>
+          <h2 className="guide__section__title"><FcAdvance /> Tu voudrais savoir comment on fait de la photo astro ? Clique sur moi pour voir ça !</h2>
         </Link>
 
       </div>
