e71bb9458bf050e5d570232a7cee486a0a2beb02
JebNewman <pierre.f.laine@gmail.com>
2021-08-23 13:46:38 +0200

    OK| css follows TODO| info tab

--- src/components/Profile/Follows.js
+++ src/components/Profile/Follows.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import mascot from 'src/assets/images/mascot-rocket.svg';
@@ -6,12 +7,14 @@ import mascot from 'src/assets/images/mascot-rocket.svg';
 export default function Follows({ users }) {
   return (
     <div className="follows">
-      {users.map((user) => (
-        <div className="follows__user" key={user.id}>
-          <img src={mascot} className="follows__user__avatar" alt={`Avatar de ${user.username}`} />
-          <span className="follows__user__username">{user.username}</span>
-        </div>
-      ))}
+      {users.length > 0
+        ? users.map((user) => (
+          <Link to={`/profile/${user.id}`} className="follows__user" key={user.id}>
+            <img src={mascot} className="follows__user__avatar" alt={`Avatar de ${user.username}`} />
+            <span className="follows__user__username">{user.username}</span>
+          </Link>
+        ))
+        : <h2>Aucun utilisateur trouvé</h2>}
     </div>
   );
 }
