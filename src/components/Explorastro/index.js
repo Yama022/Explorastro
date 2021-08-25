695e587a297a8cbb63c831ed339dd1a7b929ab31
Théo BIET <dev.theobiet@gmail.com>
2021-08-25 23:05:05 +0200

    💇 Create avatar maker component. Add 4 customizations + method for change colors

--- src/components/Explorastro/index.js
+++ src/components/Explorastro/index.js
@@ -18,6 +18,7 @@ import Profile from 'src/containers/Profile';
 import Settings from 'src/containers/Settings';
 import FormEvent from 'src/containers/FormEvent';
 import Exploration from 'src/components/Exploration';
+import Avatar from 'src/components/Avatar';
 
 import { IoIosArrowUp } from 'react-icons/io';
 
@@ -37,103 +38,103 @@ export default function Explorastro({ isLogged, checkIsLogged }) {
 
   return (
     <div className="explorastro">
-
-      {isLogged
-        ? (
-          <Switch>
-            <Route exact path="/">
-              <Redirect to="/timeline" />
-            </Route>
-            <Route exact path="/landing">
-              <Redirect to="/timeline" />
-            </Route>
-            <Route exact path="/login">
-              <Redirect to="/timeline" />
-            </Route>
-            <Route exact path="/timeline">
-              <Header />
-              <Timeline />
-              <Footer />
-            </Route>
-            <Route exact path="/exploration">
-              <Header />
-              <Exploration />
-              <Footer />
-            </Route>
-            <Route exact path="/exploration/create">
-              <Header />
-              <CreateEvent />
-              <Footer />
-            </Route>
-            <Route exact path="/discover">
-              <Header />
-              <Discover />
-              <Footer />
-            </Route>
-            <Route path="/formEvent/:id" component={FormEvent} />
-            <Route exact path="/guide">
-              <Header />
-              <Guide />
-              <Footer />
-            </Route>
-            <Route exact path="/guide/photo">
-              <Header />
-              <Photo />
-              <Footer />
-            </Route>
-            <Route exact path="/guide/visuel">
-              <Header />
-              <Visuel />
-              <Footer />
-            </Route>
-            <Route exact path="/login">
-              <Redirect to="/timeline" />
-            </Route>
-            <Route
-              path="/profile/:id"
-              render={(prop) => (
-                <>
-                  <Header />
-                  <Profile profileId={Number(prop.match.params.id)} />
-                  <Footer />
-                </>
-              )}
-            />
-            <Route exact path="/settings">
-              <Header />
-              <Settings />
-              <Footer />
-            </Route>
-            <Route>
-              <Header />
-              <Error />
-              <Footer />
-            </Route>
-          </Switch>
-        )
-        : (
-          <Switch>
-            <Route exact path="/">
-              <Redirect to="/landing" />
-            </Route>
-            <Route exact path="/landing">
-              <Header />
-              <Landing />
-              <Footer />
-            </Route>
-            <Route exact path="/login">
-              <Login />
-            </Route>
-            <Route exact path="/settings">
-              <Redirect to="/login" />
-            </Route>
-            <Route>
-              <Header />
-              <Error />
-              <Footer />
-            </Route>
-          </Switch>
-        )}
+      {isLogged ? (
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/timeline" />
+          </Route>
+          <Route exact path="/landing">
+            <Redirect to="/timeline" />
+          </Route>
+          <Route exact path="/login">
+            <Redirect to="/timeline" />
+          </Route>
+          <Route exact path="/timeline">
+            <Header />
+            <Timeline />
+            <Footer />
+          </Route>
+          <Route exact path="/exploration">
+            <Header />
+            <Exploration />
+            <Footer />
+          </Route>
+          <Route exact path="/exploration/create">
+            <Header />
+            <CreateEvent />
+            <Footer />
+          </Route>
+          <Route exact path="/discover">
+            <Header />
+            <Discover />
+            <Footer />
+          </Route>
+          <Route path="/formEvent/:id" component={FormEvent} />
+          <Route exact path="/guide">
+            <Header />
+            <Guide />
+            <Footer />
+          </Route>
+          <Route exact path="/guide/photo">
+            <Header />
+            <Photo />
+            <Footer />
+          </Route>
+          <Route exact path="/guide/visuel">
+            <Header />
+            <Visuel />
+            <Footer />
+          </Route>
+          <Route exact path="/avatar">
+            <Avatar />
+          </Route>
+          <Route exact path="/login">
+            <Redirect to="/timeline" />
+          </Route>
+          <Route
+            path="/profile/:id"
+            render={(prop) => (
+              <>
+                <Header />
+                <Profile profileId={Number(prop.match.params.id)} />
+                <Footer />
+              </>
+            )}
+          />
+          <Route exact path="/settings">
+            <Header />
+            <Settings />
+            <Footer />
+          </Route>
+          <Route>
+            <Header />
+            <Error />
+            <Footer />
+          </Route>
+        </Switch>
+      ) : (
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/landing" />
+          </Route>
+          <Route exact path="/landing">
+            <Header />
+            <Landing />
+            <Footer />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/settings">
+            <Redirect to="/login" />
+          </Route>
+          <Route>
+            <Header />
+            <Error />
+            <Footer />
+          </Route>
+        </Switch>
+      )}
       <button
         type="button"
         className="button__return__top button --secondary animate__animated animate__fadeInRight animate__faster"
