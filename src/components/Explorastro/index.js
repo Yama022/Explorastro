0525a086fdd2802b7c1d5f8590044c86636e5d79
JebNewman <pierre.f.laine@gmail.com>
2021-08-26 18:01:01 +0200

    FIX| scss backgrounds & particles

--- src/components/Explorastro/index.js
+++ src/components/Explorastro/index.js
@@ -19,8 +19,7 @@ import Settings from 'src/containers/Settings';
 import FormEvent from 'src/containers/FormEvent';
 import Exploration from 'src/components/Exploration';
 import Avatar from 'src/components/Avatar';
-
-import Particles from 'react-particles-js';
+import Particles from 'src/components/Particles';
 
 import { IoIosArrowUp } from 'react-icons/io';
 
@@ -40,118 +39,7 @@ export default function Explorastro({ isLogged, checkIsLogged }) {
 
   return (
     <div className="explorastro">
-      <Particles
-        style={{
-          position: 'fixed', zIndex: -1, top: 0, bottom: 0, left: 0, right: 0,
-        }}
-        params={
-          {
-            particles: {
-              number: {
-                value: 100,
-                density: {
-                  enable: true,
-                  value_area: 1000,
-                },
-              },
-              color: {
-                value: '#f8f8ff',
-              },
-              shape: {
-                type: 'circle',
-                stroke: {
-                  width: 0,
-                  color: '#f8f8ff',
-                },
-                polygon: {
-                  nb_sides: 5,
-                },
-              },
-              opacity: {
-                value: 1,
-                random: true,
-                anim: {
-                  enable: true,
-                  speed: 0.2,
-                  opacity_min: 0,
-                  sync: false,
-                },
-              },
-              size: {
-                value: 1.5,
-                random: true,
-                anim: {
-                  enable: false,
-                  speed: 1,
-                  size_min: 0.3,
-                  sync: false,
-                },
-              },
-              line_linked: {
-                enable: true,
-                distance: 70,
-                color: '#f8f8ff',
-                opacity: 0.05,
-                width: 1,
-              },
-              move: {
-                enable: true,
-                speed: 0.1,
-                direction: 'none',
-                random: true,
-                straight: false,
-                out_mode: 'out',
-                bounce: false,
-                attract: {
-                  enable: false,
-                  rotateX: 600,
-                  rotateY: 600,
-                },
-              },
-            },
-            interactivity: {
-              detect_on: 'window',
-              events: {
-                onhover: {
-                  enable: true,
-                  mode: 'bubble',
-                },
-                onclick: {
-                  enable: true,
-                  mode: 'push',
-                },
-                resize: true,
-              },
-              modes: {
-                grab: {
-                  distance: 400,
-                  line_linked: {
-                    opacity: 1,
-                  },
-                },
-                bubble: {
-                  distance: 250,
-                  size: 0,
-                  duration: 2,
-                  opacity: 0,
-                  speed: 3,
-                },
-                repulse: {
-                  distance: 400,
-                  duration: 0.4,
-                },
-                push: {
-                  particles_nb: 4,
-                },
-                remove: {
-                  particles_nb: 2,
-                },
-              },
-            },
-            retina_detect: true,
-          }
-    }
-      />
+      <Particles />
       {isLogged
         ? (
           <Switch>
