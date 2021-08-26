141b4b23d9a57ed76eea95e411fe619a9f38b54c
Etudiant O'clock <r-jacquemin@hotmail.fr>
2021-08-26 20:03:02 +0000

    OK | Add Redux on Follow component

--- src/components/Timeline/Follow/index.js
+++ src/components/Timeline/Follow/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 import { Link } from 'react-router-dom';
 
 import defaultAvatar from 'src/assets/images/luffy.png';
@@ -7,12 +9,19 @@ import satoru from 'src/assets/images/Satoru.jpg';
 
 import { RiUserFollowLine } from 'react-icons/ri';
 
-export default function Follow(props) {
+export default function Follow({ props, loggedUserId }) {
+  const {
+    date: { locales },
+    follower,
+    followed,
+    message,
+  } = props;
   return (
     <div className="follow">
       <div className="follow__title">
+        {/* Avatar url TO ADD ! */}
         <img src={props.avatar_url} alt="avatar" />
-        <h3> <Link to="/profile">{props.username}</Link> à commencé à suivre <Link to="/profile">Baptiste</Link></h3>
+        <h3> <Link to={`/profile/${loggedUserId}`}>{follower.username}</Link> {message.fr} <Link to="/profile">{followed.username}</Link></h3>
       </div>
 
       <div className="follow__content">
@@ -29,8 +38,17 @@ export default function Follow(props) {
       </div>
 
       <div className="follow__date">
-        <p>Le Mardi 29 Août à 13h48</p>
+        <p>{locales.fr}</p>
       </div>
     </div>
   );
 }
+
+Follow.propTypes = {
+  props: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  follower: PropTypes.objectOf(PropTypes.string).isRequired,
+  followed: PropTypes.objectOf(PropTypes.string).isRequired,
+  message: PropTypes.objectOf(PropTypes.string).isRequired,
+  loggedUserId: PropTypes.number.isRequired,
+};
