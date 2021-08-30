6e1c8590e73439088917069efc09e04e19716ad6
Théo BIET <dev.theobiet@gmail.com>
2021-08-31 01:10:55 +0200

    💬 Dynamics participatios in timeline

--- src/components/Timeline/Participation/index.js
+++ src/components/Timeline/Participation/index.js
@@ -1,30 +1,36 @@
 import React from 'react';
-
 import PropTypes from 'prop-types';
-
-import defaultAvatar from 'src/assets/images/luffy.png';
-import defaultExplo from 'src/assets/images/default-explo.jpg';
+import { Link } from 'react-router-dom';
 
 export default function JoinExplo({ props }) {
+  console.log('Participation', props);
+  const {
+    user: { id: userId, username, avatar_url: avatarURL },
+    exploration: {
+      id: explorationId, name, image_url: imageURL, date: { fr: explorationDateFr },
+    },
+    date: { fr: dateFr },
+    message: { fr: messageFR },
+  } = props;
   return (
     <div className="explo">
       <div className="explo__title">
-        <img src={defaultAvatar} alt="avatar" />
-        <h3> Toto participe à "La Nuit des Etoiles"</h3>
+        <img src={avatarURL} alt="avatar" />
+        <h3> <Link to={`/username/${userId}`}>{username}</Link> {messageFR} à <Link to={`/exploration/${explorationId}`}>"{name}"</Link></h3>
       </div>
       <div className="explo__organized">
         <div className="explo__organized--title">
-          <h3 className="explo__organized__subtitle">"La Nuit des Etoiles"</h3>
-          <p>Mardi 32 février 2021 à 20h45</p>
+          <h3 className="explo__organized__subtitle"><Link to={`/exploration/${explorationId}`}>"La Nuit des Etoiles"</Link></h3>
+          <p>{explorationDateFr}</p>
         </div>
       </div>
 
       <div className="explo__image">
-        <img src={defaultExplo} alt="Add_image" />
+        <img src={imageURL} alt="Add_image" />
       </div>
 
       <div className="explo__date">
-        <p>Le Mardi 29 Août à 13h48</p>
+        <p>{dateFr}</p>
       </div>
     </div>
   );
@@ -32,8 +38,16 @@ export default function JoinExplo({ props }) {
 
 JoinExplo.propTypes = {
   props: PropTypes.object,
+  user: PropTypes.object,
+  exploration: PropTypes.object,
+  message: PropTypes.object,
+  date: PropTypes.object,
 };
 
 JoinExplo.defaultProps = {
   props: {},
+  user: {},
+  exploration: {},
+  message: {},
+  date: {},
 };
