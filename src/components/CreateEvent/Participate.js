a92f9028f02aec492ed801c3adb8a9d9cf592ad6
JebNewman <pierre.f.laine@gmail.com>
2021-09-02 16:24:28 +0200

    FIX|scss timeline et createevent

--- src/components/CreateEvent/Participate.js
+++ src/components/CreateEvent/Participate.js
@@ -2,23 +2,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { ImMan } from 'react-icons/im';
-import { FaInfoCircle } from 'react-icons/fa';
+import { FaInfoCircle, FaRegCalendarAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import * as dayjs from 'dayjs';
 
 export default function Participate({
-  name, id, date, max_participants: maxParticipants, is_published: isPublished, image_url: imageUrl,
+  name, id, date, max_participants: maxParticipants, image_url: imageUrl,
 }) {
   return (
     <div className="createEvent__container__event-list">
       <img src={imageUrl} alt={name} />
       <h2>{name}</h2>
       <div className="createEvent__container__event-list__content">
-        <span>{date ? `date : ${dayjs(date).format('DD-MM-YY')}` : 'date :'}</span>
+        <span>{date && <> <FaRegCalendarAlt /> {dayjs(date).format('DD/MM/YYYY')} </>}</span>
         <span className="createEvent__container__event-list__content__participants">{maxParticipants}<ImMan /></span>
-      </div>
-      <div className="published">
-        <span>{isPublished ? 'Publié' : 'Non publié'}</span>
       </div>
       <div className="createEvent__container__event-list__button">
         <Link className="button --secondary" to={`/exploration/${id}`}>
@@ -37,7 +34,6 @@ Participate.propTypes = {
   id: PropTypes.number.isRequired,
   date: PropTypes.string,
   max_participants: PropTypes.number.isRequired,
-  is_published: PropTypes.bool.isRequired,
   image_url: PropTypes.string.isRequired,
 };
 
