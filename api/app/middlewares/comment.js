41d6d579d7501b1d6da81408889f647544fca8b4
Théo BIET <dev.theobiet@gmail.com>
2021-09-02 12:32:00 +0200

    🔧 Fix, delete comment && instead of ||

--- api/app/middlewares/comment.js
+++ api/app/middlewares/comment.js
@@ -15,7 +15,7 @@ module.exports = {
   checkPermissions: async (req, res, next) => {
     const comment = await Comment.findByPk(req.params.commentId);
     const adminRoleId = 3;
-    if (comment.author_id !== req.user.id || req.user.role_id !== adminRoleId) {
+    if (comment.author_id !== req.user.id && req.user.role_id !== adminRoleId) {
       return res.status(403).json({
         message: ERROR.UNAUTHORIZED,
       });
