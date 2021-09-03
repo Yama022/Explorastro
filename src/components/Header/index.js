46846ccf71aeb6c6592fffc525f45e40e307d712
Théo BIET <dev.theobiet@gmail.com>
2021-09-03 09:35:53 +0200

    ✨ Remove cursor, because he lag

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,6 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
-import { MouseContext } from 'src/context/mouse-context';
-
 import { GiHamburgerMenu } from 'react-icons/gi';
 
 import User from 'src/components/Header/User';
@@ -38,7 +36,6 @@ export default function Header({
     };
   }, []);
 
-  const { cursorChangeHandler } = useContext(MouseContext);
   return (
     <header className="header" id="header">
       <div className="header__burger" onClick={handleToggleBurger}>
@@ -60,8 +57,6 @@ export default function Header({
                 onClick={handleNavBurgerToggle}
                 to="/timeline"
                 exact
-                onMouseEnter={() => cursorChangeHandler('hovered')}
-                onMouseLeave={() => cursorChangeHandler('')}
               >
                 Fil d'actualités
               </NavLink>
@@ -71,8 +66,6 @@ export default function Header({
                 onClick={handleNavBurgerToggle}
                 to="/exploration/create"
                 exact
-                onMouseEnter={() => cursorChangeHandler('hovered')}
-                onMouseLeave={() => cursorChangeHandler('')}
               >
                 Explorations
               </NavLink>
@@ -82,8 +75,6 @@ export default function Header({
                 onClick={handleNavBurgerToggle}
                 to="/discover"
                 exact
-                onMouseEnter={() => cursorChangeHandler('hovered')}
-                onMouseLeave={() => cursorChangeHandler('')}
               >
                 Carte
               </NavLink>
@@ -93,8 +84,6 @@ export default function Header({
                 onClick={handleNavBurgerToggle}
                 to="/guide"
                 exact
-                onMouseEnter={() => cursorChangeHandler('hovered')}
-                onMouseLeave={() => cursorChangeHandler('')}
               >
                 Le guide de l'explorateur
               </NavLink>
