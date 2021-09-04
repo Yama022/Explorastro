28d3c171c8a789dfc5648169b069022eac9937fa
JebNewman <pierre.f.laine@gmail.com>
2021-09-04 12:29:53 +0200

    FIX|scss 404 OK|astrocharles 404

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -1,29 +1,38 @@
 import React, { Suspense } from 'react';
+import { Link } from 'react-router-dom';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
+
 import { BiHome } from 'react-icons/bi';
+
+import astrocharles from 'src/assets/images/mascot/mascot-moon.svg';
+
 import Mars from './Mars';
 
 export default function Error() {
   return (
     <div className="error">
-      <Canvas className="earth-model">
+      <Canvas className="mars-model">
         <Suspense fallback={null}>
           <Mars />
           <OrbitControls autoRotate />
         </Suspense>
       </Canvas>
       <div className="error__content">
-        <h2 className="animate__animated animate__fadeInDown">404</h2>
-        <p className="animate__animated animate__fadeInLeft">Cette page n'a pas été trouvée!</p>
-        <button type="button" className="button purple  animate__animated animate__fadeInRight">
+        <div className="error__content__404">
+          <h2 className="animate__animated animate__fadeInDown">4</h2>
+          <img className="error__content__404__astrocharles" src={astrocharles} alt="Astrocharles allongé sur la lune." />
+          <h2 className="animate__animated animate__fadeInDown">4</h2>
+        </div>
+        <p className="animate__animated animate__fadeInLeft">Vous vous êtes perdu dans l'espace...</p>
+        <Link to="/" type="button" className="button purple  animate__animated animate__fadeInRight">
           <span className="icon">
             <BiHome />
           </span>
           <span>
             Retourner à l'accueil
           </span>
-        </button>
+        </Link>
       </div>
     </div>
   );
