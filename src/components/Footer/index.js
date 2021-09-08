4a8e8bd40b6dd38e89894348a84dd9c7677b08a2
ƊɑѵƊɑѵ#5517 <65619418+TheoBIET@users.noreply.github.com>
2021-09-08 10:01:25 +0200

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from 'src/assets/images/logo-explorastro.png';
+import { Link } from 'react-router-dom';
 
 import background from 'src/assets/images/cropped-moon-footer.svg';
 
@@ -15,7 +16,7 @@ export default function Footer() {
     >
       <img src={logo} alt="Logo ExplorAstro" className="footer__logo" />
       <br />
-      <a href="/cgu">CONDITIONS D'UTILISATION</a>
+      <Link to="/cgu">CONDITIONS D'UTILISATION</Link>
       <div className="footer__description">
         <p className="footer__copyright">Copyright 2021 ExplorAstro</p>
       </div>
